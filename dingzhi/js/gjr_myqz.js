dee904d2f2a854a944856d1c12920b8cfbe08f94
FruitGan <1362597276@qq.com>
2017-09-28 22:24:57 +0800

    '。。。'

---
+++ dingzhi/js/gjr_myqz.js
@@ -0,0 +1,34 @@
+$(function(){
+	$('.gjr_voice').on('touchstart',function(){
+		$('body,html').addClass('hidden')
+		$('.gjr_alert').show();
+		
+	})
+	$('.gjr_alert>a').on('touchstart',function(){
+		$('.gjr_alert').hide();
+		$('body,html').removeClass('hidden')
+		
+	})
+	$('.zaixian').on('touchstart',function(){
+		$(this).parent().parent().hide();
+		$(this).parent().parent().parent().find('#gjr_active').show();
+	})
+//	$('#gjr a').on('touchstart',function(){
+//		
+//		$(this).parent().parent().hide();
+//		$(this).parent().parent().parent().find('#gjr_active').show();
+//	})
+	$('.back').on('touchstart',function(){
+		$(this).parent().parent().hide();
+		$(this).parent().parent().parent().find('#gjr').show();
+	})
+	$('.meile').on('touchstart',function(){
+		$(this).parent().parent().parent().remove()
+		
+	})
+	$('body').on('touchstart',function(e){
+		if(e.target!=$('.gjr_input').get(0)){
+			$('.gjr_input').blur()
+		}
+	})
+})
